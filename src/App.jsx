dab5545d3a6ccaf173bb4e76b5776f5b22570072
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-14 01:55:52 -0400

    Maqueta de la galería

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Favorites from "./views/Favorites";
 import Home from "./views/Home";
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,22 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 const Gallery = () => {
-  return <div className="gallery grid-columns-5 p-3"></div>;
-};
+  return(
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+
 export default Gallery;
