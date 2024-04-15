86957cc0e59c7a55abd910b9d6258f6d4c2358ac
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-15 17:49:31 -0400

    create favorites

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,9 +1,25 @@
+import { useContext } from 'react';
+import { PhotosContext } from '../context/PhotosContext';
+import Gallery from "../components/Gallery";
+
 const Favorites = () => {
+  const { photos } = useContext(PhotosContext)
+  console.log(photos)
+
   return (
-    <div>
+    <>
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
-    </div>
+      <div className='container'>
+        <div className="row">
+
+          {photos &&
+           photos.filter(photo => photo["liked"]).map((photo) => (
+            <Gallery key={photo.id} photo={photo} />
+
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 export default Favorites;
