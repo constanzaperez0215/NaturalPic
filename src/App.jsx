ef011137425386ef0e1247ddc87492d571dffc01
Constanza Pérez <constanza.vp.o@gmail.com>
2024-04-16 12:34:34 -0400

    Natural Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Favorites from "./views/Favorites";
 import Home from "./views/Home";
+import NotFound from "./views/NotFound";
 
 
 const App = () => {
@@ -13,12 +14,16 @@ const App = () => {
 
       <Routes>
         <Route
-          path="/"
+          path="/NaturalPic"
           element={<Home />}
         />
         <Route
           path="/favoritos"
           element={<Favorites />}
+        />
+        <Route
+          path="/notFound"
+          element={<NotFound />}
         />
       </Routes>
     </div>
